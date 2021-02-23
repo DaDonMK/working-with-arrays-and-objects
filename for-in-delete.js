ea0f79fbc6c5630be4d9cdf19e7d6a3291d41307
Mustafa Khan <mustafakhan98@gmail.com>
2021-02-23 06:07:28 +0300

    changes

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,13 +11,13 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
 // for(var key in values) {
 //   console.log(values[key])
@@ -28,8 +28,8 @@
 */
 
 // for(var key in values) {
-//   console.log(key)
-// }
+//    console.log(key)
+//  }
 
 
 
@@ -38,9 +38,12 @@
 /*
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
-
+let s = ""
 function showValues( obj ) {
-  //Code Here
+  for(var key in obj){
+    s += obj[key]
+  }
+  return s
 }
 
 
@@ -53,8 +56,15 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function greaterThan10(obj){
 
+  for(var key in obj){
+    if(obj[key] > 10){
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -65,8 +75,13 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function double(obj){
 
+  for(var key in obj){
+    obj[key] *= 2
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -79,8 +94,12 @@ function showValues( obj ) {
   By the end of the for in loop, you should have a sentence, return that sentence.
 */
 
-//Code Here
-
+function secrets(obj){
+  let s = ""
+  for(var key in obj){
+    
+  }
+}
 
 
 /* 
